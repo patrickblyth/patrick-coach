ac8c5bbee90fcd7e20f7baa0b9da4e45b650b2c8
patrickblyth <79188731+patrickblyth@users.noreply.github.com>
2026-05-18 10:57:31 +1000

    Rename BackoffClient to BackOffHTTPClient

--- strava_backfill.py
+++ strava_backfill.py
@@ -111,7 +111,7 @@ def get_strava_access_token() -> str:
 def get_gspread_client() -> gspread.Client:
     sa_info     = json.loads(os.environ["GOOGLE_SERVICE_ACCOUNT"])
     creds       = Credentials.from_service_account_info(sa_info, scopes=GSHEETS_SCOPES)
-    http_client = gspread.BackoffClient(auth=creds)
+    http_client = gspread.BackOffHTTPClient(auth=creds)
     return gspread.Client(auth=creds, http_client=http_client)
 
 
